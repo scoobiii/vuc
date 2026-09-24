b758eb87dba512e04d32bcb8834647168676f56c
scoobiii <sobrinhosj@gmail.com>
2026-09-24 03:24:30 -0300

    fix(android): use dependency-free Capacitor config

---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+const config = {
+  appId: 'br.eco.mex.vuc',
+  appName: 'VUC',
+  webDir: 'dist',
+  server: { androidScheme: 'https' },
+  android: { allowMixedContent: false },
+};
+
+module.exports = config;
